f475eb31192031a60ca67aee6c0aed9988a3db0b
David Huang <daveshuang@gmail.com>
2019-07-08 16:27:17 -0700

    selecting id, name, and cost from database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ const inquirer = require('inquirer');
 class CustomerOrder {
     constructor(host = process.env.DB_HOST, port = process.env.DB_PORT, user = process.env.DB_USER, password = process.env.DB_PASS, database = process.env.DB) {
         // Product array
-        this.products = [];
+        this._products = [];
         // Database connection variable
         this._connection = mysql.createConnection({
             host: host,
@@ -30,13 +30,25 @@ class CustomerOrder {
     displayProducts() {
         this._connection.query('SELECT item_id, product_name, price FROM products', (err, res) => {
             if (err) throw err;
-            console.log(res);
+            //console.log(res);
+
+            const table = [];
+            for(let i = 0; i < res.length; i++) {
+                table.push(
+                    {
+                        'ID': res[i].item_id,
+                        'Product': res[i].product_name,
+                        'Price': `$${res[i].price}`
+                    }
+                )
+            }
+            console.log(table);
         });
     }
 
     disconnect() {
-        console.log('\nThanks for Shopping with BAMazon!\n');
         this._connection.end();
+        console.log('\nThanks for Shopping with BAMazon!\n');
     }
 
 }
@@ -49,16 +61,10 @@ class CustomerOrder {
 // newOrder.disconnect(); // need promise to delay this until after display completes
 
 
-const main = async () => {
+const main = () => {
     const newOrder = new CustomerOrder();
-    await newOrder.displayProducts();
+    newOrder.displayProducts();
     newOrder.disconnect();
 }
 
 main();
-
-
-// newOrder.connection.connect(err => {
-//     if(err) throw err;
-//     console.log("connected as id " + newOrder.connection.threadId + "\n");
-// })
