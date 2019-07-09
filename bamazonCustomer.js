51b094c7bbcf80b5608040fa43a339a736ef5c2a
David Huang <daveshuang@gmail.com>
2019-07-08 21:58:35 -0700

    added async await

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,13 +2,15 @@
 require('dotenv').config();
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const {Table} = require('console-table-printer');
+const { Table } = require('console-table-printer');
 
 /**
  * Customer Order Class
  */
 class CustomerOrder {
     constructor(host = process.env.DB_HOST, port = process.env.DB_PORT, user = process.env.DB_USER, password = process.env.DB_PASS, database = process.env.DB) {
+        // Number of items in database, length
+        this._numItems = 0;
         // Database connection variable
         this._connection = mysql.createConnection({
             host: host,
@@ -32,35 +34,48 @@ class CustomerOrder {
     }
 
     /**
-     * @desc Displays items from database ordered by id, item, price
+     * @desc Getter for length
+     */
+    get numItems() {
+        return this._numItems;
+    }
+
+    /**
+     * @desc Displays items from databaseint a table ordered by id, item, price
      */
     displayProducts() {
-        this._connection.query('SELECT item_id, product_name, price FROM products', (err, res) => {
-            if (err) throw err;
-            
-            // Creates new table object to hold data to display
-            const productTable = new Table({
-                style: 'fatborder',
-                columns: [
-                    {name: 'ID', alignment: 'right'},
-                    {name: 'Item', alignment: 'left'},
-                    {name: 'Price', alignment: 'right'}
-                ]
+        return new Promise((resolve, reject) => {
+            this._connection.query('SELECT item_id, product_name, price FROM products', (err, res) => {
+                if (err) reject(err);
+
+                // Set length
+                this._numItems = res.length;
+                //console.log(this._numItems);
+
+                // Creates new table object to hold data to display
+                const productTable = new Table({
+                    style: 'fatborder',
+                    columns: [
+                        { name: 'ID', alignment: 'right' },
+                        { name: 'Item', alignment: 'left' },
+                        { name: 'Price', alignment: 'right' }
+                    ]
+                });
+
+                // Loops through results and adds relevant data to table for display
+                for (let i = 0; i < this._numItems; i++) {
+                    productTable.addRow(
+                        {
+                            'ID': res[i].item_id,
+                            'Item': res[i].product_name,
+                            'Price': `$${res[i].price.toFixed(2)}`
+                        }
+                    )
+                }
+
+                // Logs table to console
+                resolve(productTable.printTable());
             });
-
-            // Loops through results and adds relevant data to table for display
-            for(let i = 0; i < res.length; i++) {
-                productTable.addRow(
-                    {
-                        'ID': res[i].item_id,
-                        'Item': res[i].product_name,
-                        'Price': `$${res[i].price.toFixed(2)}`
-                    }
-                )
-            }
-
-            // Logs table to console
-            productTable.printTable(productTable);
         });
     }
 
@@ -69,7 +84,7 @@ class CustomerOrder {
      */
     disconnect() {
         this._connection.end();
-        console.log('\nThanks for Shopping with BAMazon!\n');
+        console.log('\nThank you for Shopping with BAMazon!\n');
     }
 
 }
@@ -82,10 +97,20 @@ class CustomerOrder {
 // newOrder.disconnect(); // need promise to delay this until after display completes
 
 
-const main = () => {
+/**
+ * Main
+ */
+const main = async () => {
+
+    // Creates a new order
     const newOrder = new CustomerOrder();
-    newOrder.displayProducts();
+    await newOrder.displayProducts();
+
+
+
+    // console.log('awe', newOrder.numItems);
     newOrder.disconnect();
 }
 
+// main function call
 main();
